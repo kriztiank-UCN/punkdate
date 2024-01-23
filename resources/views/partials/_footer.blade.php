7fa1d3bc94bd14d21d6c42b78bd4452b67f19d53
Kristian <10221425@ucn.dk>
2024-01-23 07:55:27 +0100

    Z-Index on footer, bring to front

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer
-class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
-<p class="ml-2">&copy; 2023 PunkDate</p>
+    class="z-10 fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 md:justify-center">
+    <p class="ml-2">&copy; 2023 PunkDate</p>
 
-<a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Create Profile</a>
+    <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Create Profile</a>
 </footer>
